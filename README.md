9a36a6ced0de2c55bdbb9555a26fec7542380667
luandro <luandro@gmail.com>
2017-08-10 11:44:48 -0300

    added default playlist and options

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Automatically downloads and conversts to mp3 the youtube mix playlist for any nu
 
 ## How it works
 
-It adds the keyword "mix" to the artist name, searches on Youtube filtering only playlists and downloads the first result playlist with `youtube-dl`, extracting audio and converting it to `mp3` format. Automatically adds `--no-overwrites`, `--continue` and `--ignore-errors` options. Also uses `--min-views` options to only download videos with at least 1000 views. 
+It adds the keyword "mix" to the artist name, searches on Youtube filtering only playlists and downloads the first result playlist with `youtube-dl`, extracting audio and converting it to `mp3` format.
+
+Automatically adds `--no-overwrites`, `--continue` and `--ignore-errors` options. Also uses `--min-views` options to only download videos with at least 1000 views. 
 
 ## Usage
+
+Make sure you have NodeJS at least v8.
 
 ```
 npm i -g mix-dl
@@ -18,16 +22,28 @@ or
 yarn global add mix-dl
 ```
 
+That's it. Now just use it like this:
+
+```
+mix-dl [ARTISTS] [OPTIONS]
+```
+
 Example:
 
 ```mix-dl "Bob Marley" "Charlie Brown Jr" -n 20 ```
 
 Where `-n` is the number of songs per artist, which defaults to `30`.
 
+### Options
+
+- `n` `INT` - number of songs to download defaults to 25
+- `f` `STRING` - folder name to download to mix-downloads
+- `v` - verbose mode on defaults to false
+- `m` `INT` - minimum views defaults to 1000
+- `d` - debug mode on defaults to false
+
 ## Todo
 
-- Add default playlist
-- Add options
 - Use `--match-title` option to only download songs containing artist name
 - Download each artist to it's own folder
 - Add better interface
--- index.js
+++ index.js
@@ -9,25 +9,36 @@ const flatten = require('flatten-array');
 const fs = require('fs');
 const path = require('path');
 
-// defaults to https://www.youtube.com/watch?v=qTHVZKn1398
+// Debug
+const debugList = 'PLmZ-tcwzKMWbRRP8RbqtG4jA51ULwvBkj';
 
 // Get Mix url from scrapping Youtube
 console.log('Loading...')
 
-const findAndDownloadMix = n => {
-  console.log('Downloading ', n)
-  scrapeForMixes().then(mixArray => {
+const findAndDownloadMix = (artists, numberOfSongs, folderName, verbose, minimumViews, debugMode) => {
+  console.log('Downloading ', numberOfSongs);
+  scrapeForMixes(artists, debugMode).then(mixArray => {
     // Download with yotube-dl
-    const songFolder = `${__dirname}/Download`;
+    const songFolder = `${__dirname}/${folderName}`;
     console.log('Downloading to ', songFolder);
     if (!fs.existsSync(songFolder)) {
       fs.mkdirSync(songFolder);
     }
+
+    const youtubedlOptions = [];
+
+    verbose && youtubedlOptions.push('-v') && console.log('Verbose on: ', verbose);
+    if (debugMode) {
+      // youtubedlOptions.push('--flat-playlist');
+      console.log(parseArgs, 'Debug mode on: ', debugMode);
+    } else {
+      youtubedlOptions.push('--min-views', 1000);
+    }
     const downloadMix = spawn('youtube-dl', flatten([
+      youtubedlOptions,
       '--playlist-items',
-      n,
-      '-v',
-      '--write-pages',
+      ` 0 - ${parseInt(numberOfSongs) - 1}`,
+      // '--write-pages',
       '--output',
       `${songFolder}/%(title)s.%(ext)s`,
       // '--print-json',
@@ -37,8 +48,6 @@ const findAndDownloadMix = n => {
       '--no-overwrites',
       '--continue',
       '--ignore-errors',
-      '--min-views',
-      '1000',
       mixArray,
     ]));
     downloadMix.stdout.on('data', (data) => {
@@ -56,17 +65,23 @@ const findAndDownloadMix = n => {
   });
 }
 
-async function scrapeForMixes() {
-  let list = [];
-  for (let artist of parseArgs._) {
-    if (artist !== undefined) {
-      const formatedArtist = artist.split(' ').concat('+mix&sp=EgIQA1AU').join('+');
-      const info = await scrapeEachArtist(formatedArtist)
-      list.push(info);
+async function scrapeForMixes(artists, debugMode) {
+  if (artists.length > 0) {
+    let list = [];
+    for (let artist of artists) {
+      if (artist !== undefined) {
+        const formatedArtist = artist.split(' ').concat('+mix&sp=EgIQA1AU').join('+');
+        const info = await scrapeEachArtist(formatedArtist)
+        list.push(info);
+      }
     }
+    debugMode && console.log(list);
+    return list;
+  } else if (debugMode){
+    return [`https://www.youtube.com/watch?v=bjOLXn3mz-Y&list=${debugList}`];
+  } else {
+    return ['https://www.youtube.com/watch?v=qTHVZKn1398']
   }
-  console.log(list)
-  return list;
 }
 
 async function scrapeEachArtist(artist) {
@@ -82,4 +97,10 @@ async function scrapeEachArtist(artist) {
   return `https://www.youtube.com${result.data}`;
 }
 
-findAndDownloadMix(parseArgs.n || 25);
+const { n, f, v, m, d, _ } = parseArgs;
+// n INT - number of songs to download defaults to 25
+// f STRING - folder name to download to mix-downloads
+// v - verbose mode on defaults to false
+// m INT - minimum views defaults to 1000
+// d - debug mode on defaults to false
+findAndDownloadMix(_, n || 25, f || 'mix-downloads', v || false, m || 1000, d || false);
